0bae43698680604a74142778565122d6ec08a24c
SunilG <sc3ways@gmail.com>
2025-06-27 09:06:52 +0530

    heading commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="container lg:w-5xl sm:w-full mx-auto">
         <div className="lg:flex justify-center sm:block flex-wrap py-5 ">
           <div className="sec-title w-full mb-5 px-3">
-            <h1 className="capitalize font-semibold">Track your expense</h1>
+            <h1 className="capitalize font-semibold">Track your expenses</h1>
           </div>
           <div className="lg:w-[50%] sm:w-[100%] xs:w-[100%] p-3">
             <ExpenseForm />
